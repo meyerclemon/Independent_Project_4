3a20430b445a690b6d1c6ce6f1b5abc22d331b64
Maya Frame <mayacframe@gmail.com>
2019-04-12 10:57:46 -0700

    add initial js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
+function PizzaOrder(total, options, size) {
+  this.total = total;
+  this.options=option;
+  this.size =size;
+}
+
+PizzaOrder.prototype.calculateTotal = function(total) {
+  size = sizePrice;
+  toppings = [];
+}
+
+PizzaOrder.prototype.addSize = function(size) {
 
 
+
+}
+};
 // Front
 $(document).ready(function(){
   $("form#send-order").click(function(event){
+    var size=$("#size");
+    var topping=$("#option");
+    var pizza = new PizzaOrder(size,topping);
+    pizza.addSize(size);
+
+
+
+var toppingsPrice = 0;
+  $("input:checkbox[name=toppings]:checked").each(function() {
+    toppingsCounter += 2;
+  })
+var sizePrice =  $("input:radio[name=size]:checked").val();
+
+var sauce = $("#sauce").val();
+}
+
+};
+});
