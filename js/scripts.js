5d6adc19cb8de87d1195e38f3d85236aa8c4ecf4
Maya Frame <mayacframe@gmail.com>
2019-04-12 15:47:30 -0700

    add js debugging

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,43 @@ function PizzaOrder(total, toppings, size) {
   this.size =size;
 }
 
-
 PizzaOrder.prototype.addSize = function() {
-if (this.size == "small"){
+if (this.size === "small"){
   this.total += 10;
 }
-  else if(this.size == "medium" ){
+  else if(this.size === "medium" ){
   this.total += 15;
 }
   else {
   this.total += 20;
 }
 };
-
+function Tops() {
+  if(toppings === 1) {
+    total = 2;
+  // } else {
+  //   total =+ toppings;
+  // }
+}
+var toppings = $("input:checkbox[name=toppings]:checked").each(function() {
+});
 var totalPizza = function (object) {
   $("total").val(object.total);
   $("toppings").val(object.toppings);
   $("size").val(object.size);
+
 };
 // Front
 $(document).ready(function(){
-  $("form#pizza-order").submit(function(event){
-alert("hi");
+  $("form#pizza-order").click(function(event){
     event.preventDefault();
     var size =  $("input:radio[name=size]:checked").val();
-    var toppings = $("input:checkbox[name=toppings]:checked").each(function() {
-    toppings += 2;
-  });
-
+    Tops();
     var total = 0;
     var newPizza = new PizzaOrder(size,toppings,total);
-    newPizza.addSize(size);
-    newPizza.toppings(toppings);
-    newPizza.total(total);
-
-    $("#output").text("Your total is: $" + totalPizza);
+    newPizza.addSize();
+    this.toppings();
+alert("almost there");
+    $("#output").text("Your total is: $" + totalPizza.total);
 });
 });
